3ef1d071b7666124a05b99b08ea1687ce3a7868d
jongro <jongro@users.noreply.github.com>
2017-05-29 23:31:24 +0200

    neu

--- src/js.js
+++
@@ -1,9 +0,0 @@
-$(window).on('load resize', function () {
-    lightbox.option({
-        'alwaysShowNavOnTouchDevices': true,
-        'showImageNumberLabel': false,
-        'resizeDuration': 400,
-        'imageFadeDuration': 400,
-        'fadeDuration': 800,
-    });
-});
